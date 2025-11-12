c1c7797db13f7eff7d83335fc9ac54fddd5216ce
OmarIsmail2019 <OmarIsmail2019@github.com>
2025-11-12 13:28:38 +0200

    Initial Commit

---
+++ EKYC/EKYCApp.swift
@@ -0,0 +1,17 @@
+//
+//  EKYCApp.swift
+//  EKYC
+//
+//  Created by MacBookPro on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct EKYCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
